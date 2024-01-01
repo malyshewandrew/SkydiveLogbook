fd12fbae08ed2d68ec1f41a89362593c4d003f56
malyshewandrew <malyshewandrew@protonmail.com>
2024-01-01 15:26:16 +0300

    finish edit feature "Account"

--- Skydive Logbook/CORE DATA/arrayAccount.swift
+++ Skydive Logbook/CORE DATA/arrayAccount.swift
@@ -1,6 +1,8 @@
 import Foundation
 
-var arrayAccount: [AccountStructure] = [AccountStructure(nickname: "Malyshew Andrew",
+var arrayAccount: [AccountStructure] = [
+    AccountStructure(nickname: "Malyshew Andrew",
                                                          medicine: "21.12.2021",
                                                          insurance: "05.10.2023",
-                                                         reserveCanopy: "14.07.2015")]
+                                                         reserveCanopy: "14.07.2015")
+]
